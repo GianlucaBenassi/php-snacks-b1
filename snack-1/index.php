c05559dabce49ef927ade6983827e15b518f10c7
Gianluca Benassi <benox1199@gmail.com>
2022-01-18 13:46:43 +0100

    Snack-1 array

---
+++ snack-1/index.php
@@ -0,0 +1,32 @@
+<?php
+// Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+// Olimpia Milano - Cantù | 55-60
+
+$games= [
+    [
+        'homeTeam' => 'Hawks',
+        'homePoint' => 121,
+        'visitingTeam' => 'Bucks',
+        'visitingPoint' => 114
+    ],
+    [
+        'homeTeam' => 'Magic',
+        'homePoint' => 88,
+        'visitingTeam' => 'Trail Blazers',
+        'visitingPoint' => 98
+    ],
+    [
+        'homeTeam' => 'Heat',
+        'homePoint' => 104,
+        'visitingTeam' => 'Raptors',
+        'visitingPoint' => 99
+    ],
+    [
+        'homeTeam' => 'Spurs',
+        'homePoint' => 107,
+        'visitingTeam' => 'Suns',
+        'visitingPoint' => 121
+    ]
+];
+
+?>
